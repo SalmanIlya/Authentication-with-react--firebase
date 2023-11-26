22eccb34737cb0374d2d076a276c07d54e18dd32
salamanilyas <salamanilyas101@gmail.com>
2023-11-26 23:27:22 +0500

    add-blogs-in-routes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ import Register from "./Pages/Register";
 import Home from "./Pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import CreateBlog from "./Pages/CreateBlog";
 
 const App = () => {
-
   return (
     <Routes>
-      <Route path="/" exect element={
-        <div>
-          <Navbar/>
-        <Home/>
-<Footer/>
-        </div>
-      } />
-      <Route path="/Login" exect element={<Login/>} />
-      <Route path="/Register" exect element={<Register/>}/>
-       <Route
+      <Route
+        path="/"
+        exect
+        element={
+          <div>
+            <Navbar />
+            <Home />
+            <Footer />
+          </div>
+        }
+      />
+        <Route
+        path="/createblog"
+        exect
+        element={
+          <div>
+            <Navbar />
+            <CreateBlog />
+            <Footer />
+          </div>
+        }
+      />
+      <Route path="/Login" exect element={<Login />} />
+      <Route path="/Register" exect element={<Register />} />
+      <Route
         path="*"
         exect
         element={
