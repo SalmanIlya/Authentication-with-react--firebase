56ab35abcb2541c09383c76f87403d62faf42d60
salamanilyas <salamanilyas101@gmail.com>
2023-11-26 22:02:18 +0500

    some-default-files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" exect element={
+        <div>
+          <Navbar/>
+        <Home/>
+<Footer/>
+        </div>
+      } />
+      <Route path="/Login" exect element={<Login/>} />
+      <Route path="/Register" exect element={<Register/>}/>
+       <Route
+        path="*"
+        exect
+        element={
+          <div className="h-screen flex flex-row justify-center items-center">
+            <h1>404 Page not Found.</h1>
+          </div>
+        }
+      />
+    </Routes>
   );
-}
+};
 
 export default App;
