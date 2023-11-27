65b5e69f539ffaf9f6ac332d2bab29b87d924e8a
salamanilyas <salamanilyas101@gmail.com>
2023-11-27 19:43:48 +0500

    small-changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import { Usefirebase } from '../Context/Firebase'
+import Card from '../components/Card'
 const Home = () => {
+  const firebase=Usefirebase()
+  const [Blogs, setBlogs] = useState([])
+
+  useEffect(()=>{
+   
+    firebase.getAllBlogs().then((res)=>{
+      setBlogs(res.docs)
+    })
+  },[firebase])
+  if(Blogs==[]){
+    return <div className='flex flex-col justify-center items-center '><h1>Loading...</h1></div>
+  }
   return (
-    <div className='h-screen '>Home</div>
+    <div>
+
+<h1 className='text-center text-4xl font-bold m-5 '>All Blogs</h1>
+    <div className=' min-h-screen max-h-auto flex flex-row justify-center items-center flex-wrap '>
+  
+     {
+      Blogs.map((item)=>{
+   const data=item.data()
+console.log(item.id);
+        return(
+          <div key={data.date}  className=' flex flex-row justify-center items-center'>
+         <Card data={data} id={item.id} />
+          </div>
+        )
+      })
+     }
+    </div>
+    </div>
+
   )
 }
 
