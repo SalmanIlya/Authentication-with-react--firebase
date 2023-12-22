727a83b1a34374c8f78bf3eff88fba3b95c969f5
salamanilyas <salamanilyas101@gmail.com>
2023-12-22 15:14:30 +0500

    create-herosection

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,12 +18,22 @@ const Home = () => {
     );
   }
   return (
-    <div>
-      <h1 className="text-center text-4xl font-bold m-5 ">All Blogs</h1>
+    <div className="bg-gray-200 pb-20">
+      <div className="h-screen flex  2xl:flex-row xl:flex-row lg:flex-row md:flex-row sm:flex-col-reverse flex-col-reverse justify-between items-center px-20 bg-gray-200 py-10">
+        <div className=" 2xl:w-1/2 xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-1/2  w-full">
+          <h1 className=" 2xl:text-6xl xl:text-6xl lg:text-6xl  
+          md:text-4xl sm:text-3xl text-2xl
+          font-extrabold">Lorem ipsum dolor sit amet consectetur </h1>
+          <p className="text-sm mt-3 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores facere blanditiis ex. Quos impedit repellendus quasi libero saepe itaque delectus!</p>
+        </div>
+        <img src="./images/1.png" alt="" className="h-2/3 image-h" />
+      </div> 
+     
+      <h1 className="text-center text-4xl font-extrabold  my-20">All Blogs</h1>
       <div className=" min-h-screen max-h-auto flex flex-row justify-center items-center flex-wrap ">
         {Blogs.map((item) => {
           const data = item.data();
-          console.log(item.id);
+        
           return (
             <div
               key={data.date}
